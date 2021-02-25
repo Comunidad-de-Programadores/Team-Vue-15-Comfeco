67e069550ba55b18bc7011edd4179db15f2acb9b
Luis <lagcamgc@gmail.com>
2021-02-24 20:38:43 -0400

    Prototipo de uso para carousel

--- components/Slider1.vue
+++ components/Slider1.vue
@@ -1,17 +1,95 @@
 <template>
   <div id="Slider1">
-    <h1>slider 1</h1>
+    <!-- <h1>slider 1</h1> -->
+    <VueSlickCarousel  v-bind="settings">
+
+        <template #prevArrow="arrowOption">
+          <div class="custom-arrow">
+            {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+          </div>
+        </template>
+        
+        <template #nextArrow="arrowOption">
+          <div class="custom-arrow">
+            {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+          </div>
+        </template>
+
+        <div
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+
+    </VueSlickCarousel>
+
   </div>
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 import '~/assets/css/Slider1.css'
 
-export default {
 
+export default {
+  components: { VueSlickCarousel },
+    data() {
+      return {
+        settings: {
+          "autoplay": true,
+          "dots": true,
+          "focusOnSelect": true,
+          "infinite": true,
+          "speed": 500,
+          "slidesToShow": 3,
+          "slidesToScroll": 3,
+          "touchThreshold": 5,
+        },
+        cards: [
+          { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+          { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+          { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+          { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+          { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+          { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+        ],
+      };
+    },
 }
 </script>
 
 <style scoped>
-
+.custom-arrow {
+  display: none
+}
 </style>
--- components/Slider2.vue
+++ components/Slider2.vue
@@ -4,19 +4,83 @@
       <h3>Sponsors</h3>
       <button>Apoyar iniciativa</button>
     </div>
-    Slider2
+    
+    <VueSlickCarousel  v-bind="settings">
+      <template #prevArrow="arrowOption">
+        <div class="custom-arrow">
+          {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+        </div>
+      </template>
+      <template #nextArrow="arrowOption">
+        <div class="custom-arrow">
+          {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+        </div>
+      </template>
+      <div>
+        <v-avatar>
+          <img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          >
+        </v-avatar>
+      </div>
+      <div>
+        <v-avatar>
+          <img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          >
+        </v-avatar>
+      </div>
+      <div>
+        <v-avatar>
+          <img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          >
+        </v-avatar>
+      </div>
+      <div>
+        <v-avatar>
+          <img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          >
+        </v-avatar>
+      </div>
+    </VueSlickCarousel>
+
   </div>
 
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 import '~/assets/css/Slider2.css'
 
 export default {
-
+  components: { VueSlickCarousel },
+  data() {
+    return {
+      settings: {
+        "dots": true,
+        "focusOnSelect": true,
+        "infinite": true,
+        "speed": 500,
+        "slidesToShow": 2,
+        "slidesToScroll": 3,
+        "touchThreshold": 5
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
+.custom-arrow {
+  background: black
+}
 </style>
